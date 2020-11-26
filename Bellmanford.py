c6fc7f4722df35844a805df901a51637e45005e7
Sami Sh <sami_sh99@outlook.com>
2020-11-26 15:16:12 +0200

    Modify Bellman

--- Bellmanford.py
+++ Bellmanford.py
@@ -1,3 +1,7 @@
+import os
+from DAG_Generator import load
+from collections import deque
+import networkx as nx
 def initialize(graph, source):
     d = {} # Stands for destination
     p = {} # Stands for predecessor
@@ -19,21 +23,73 @@ def backtrace(prev, start, end):
     path = []
     while node != start:
         path.append(node)
-        node = prev[node]
+        node = prev[node][0]
     path.append(node) 
     path.reverse()
     return path
 
-def bellman_ford(graph, source,sink):
-    d, p = initialize(graph, source)
-    for i in range(len(graph)-1): #Run this until is converges
-        for u in graph:
-            for v in graph[u]: #For each neighbour of u
-                relax(u, v, graph, d, p) #Lets relax it
+def bellman_ford(G, source,target,pred=None, paths=None, dist=None):
+    if source not in G:
+        raise nx.NodeNotFound("Source {} not in G".format(source))
 
-    # Step 3: check for negative-weight cycles
-    # for u in graph:
-    #     for v in graph[u]:
-    #         assert d[v] <= d[u] + graph[u][v]['weight']
+    if pred is None:
+        pred = {source:[]}
 
-    return d, backtrace(p,source,sink)
+    if dist is None:
+        dist = {source:0}
+
+    G_succ = G.succ if G.is_directed() else G.adj
+    inf = float(0)
+    n = len(G)
+
+    count = {}
+    q = deque([source])
+    in_q = set([source])
+    while q:
+        u = q.popleft()
+        in_q.remove(u)
+
+        # Skip relaxations if any of the predecessors of u is in the queue.
+        if all(pred_u not in in_q for pred_u in pred[u]):
+            dist_u = dist[u]
+            for v, e in G_succ[u].items():
+                dist_v = dist_u + G_succ[u][v]['weight']
+
+                if dist_v > dist.get(v, inf):
+                    if v not in in_q:
+                        q.append(v)
+                        in_q.add(v)
+                        count_v = count.get(v, 0) + 1
+                        if count_v == n:
+                            raise nx.NetworkXUnbounded(
+                                "Negative cost cycle detected.")
+                        count[v] = count_v
+                    dist[v] = dist_v
+                    pred[v] = [u]
+
+                elif dist.get(v) is not None and dist_v == dist.get(v):
+                    pred[v].append(u)
+
+    if paths is not None:
+        dsts = [target] if target is not None else pred
+        for dst in dsts:
+
+            path = [dst]
+            cur = dst
+
+            while pred[cur]:
+                cur = pred[cur][0]
+                path.append(cur)
+
+            path.reverse()
+            paths[dst] = path
+
+    return backtrace(pred,source,target)
+
+
+if __name__=='__main__':
+    
+    fnames=os.listdir('data')
+    G = load('data/%s'%fnames[0])
+    print("Applying Bellmanford Algorithm")
+    print(bellman_ford(G,0,len(G.nodes())-1))
