518ae7d5a8817d8f3e4dca721cae1eef4d5a7103
Sami Sh <sami_sh99@outlook.com>
2020-12-08 06:37:24 +0200

    Refactor Mains

--- main_edges.py
+++ main_edges.py
@@ -2,46 +2,52 @@ import matplotlib.pyplot as plt
 import networkx as nx
 import time
 from Algorithms.Dijkstra import dijkstra
+from Algorithms.FordFulkerson import ford_fulkerson
 from Algorithms.Bellmanford import bellman_ford
 from Algorithms.TopologicalSort import topological_sort
-from Generator.DAG_Generator import load
+from Generator.DAG_Weight_Generator import load
 import os
 
 def parse_file(x:str):
     return int(x[x.find('_')+1:x.find('.')])
 
-fnames=os.listdir('data')
+fnames=os.listdir('dataE')
 
 D_V=dict()
 B_V=dict()
 T_V=dict()
+MB_V=dict()
 
 for fname in fnames:
     print("loading %s"%fname)
-    G = load('data/%s'%fname)
-    G_neg = load('data/%s'%fname,True)
+    G = load('dataE/%s'%fname)
+    G_neg = load('dataE/%s'%fname,True)
 
     print("Applying Modified Dijkstra Algorithm for %s"%fname)
     start_time = time.time()
-    longest_path_D=dijkstra(G,0,len(G.nodes())-1)
+    shortest_path_D=dijkstra(G,0,len(G.nodes())-1)
     elapsed_time = time.time() - start_time
     D_V[parse_file(fname)]=float(elapsed_time)
 
     print("Applying Topological Sort Algorithm for %s"%fname)
     start_time = time.time()
-    longest_path_T=nx.topological_sort(G)
+    shortest_path_T=nx.dag_longest_path(G)
     elapsed_time = time.time() - start_time
     T_V[parse_file(fname)]=float(elapsed_time)
 
     print("Applying Bellman Ford Algorithm for %s"%fname)
     start_time = time.time()
-    longest_path_B=bellman_ford(G,0,len(G.nodes())-1)
+    shortest_path_B=bellman_ford(G,0,len(G.nodes())-1)
     elapsed_time = time.time() - start_time
     B_V[parse_file(fname)]=float(elapsed_time)
 
+    # print("D:",shortest_path_D)
+    # print("T:",shortest_path_T)
+    # print("B:",shortest_path_B)
+
 
 plt.title("Time to compute Longest-Path")
-plt.xlabel("Number of Vetrices")
+plt.xlabel("Number of Edges")
 plt.ylabel("Time taken (seconds)")
 plt.plot(list(D_V.keys()),list(D_V.values()))
 plt.plot(list(B_V.keys()),list(B_V.values()))
